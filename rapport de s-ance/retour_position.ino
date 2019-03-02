949c0696728af33d441ded8aaaed7036aedeee7d
quentinhqn <45876953+quentinhqn@users.noreply.github.com>
2019-03-02 20:16:29 +0100

    Update retour_position.ino

--- rapport de s-ance/retour_position.ino
+++ rapport de s-ance/retour_position.ino
@@ -1,9 +1,6 @@
-//capteur 1
-int TRIG1=2;
-int ECHO1=8;
-//capteur 2
-int TRIG2=12;
-int ECHO2=13;
+#include <NewPing.h>
+float cm1=0;
+float cm2=0;
 
 //Moteur stylé
 int ENA = 9;
@@ -18,24 +15,44 @@ int IN4 = 7;
 
 double i=0;
 double j=0;
+
+
 void setup() {
   Serial.begin(9600);
-
+    //on configure en sorties
+  pinMode(ENA,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  //pareil pour moteur rouge
+  pinMode(ENB,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+//on dit au moteurs de ne pas tourner
+  digitalWrite(ENA,LOW);
+  digitalWrite(ENB,LOW);
+//on donne la direction de départ du moteur stylé
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+//pareil pour moteur rouge
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
 }
 
 void loop() {
   
-  analogWrite(ENA,50);
-  digitalWrite(TRIG1,HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG1,LOW);
-    i=pulseIn(ECHO1,HIGH);
-
-    digitalWrite(TRIG2,HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG2,LOW);
-    j=pulseIn(ECHO2,HIGH);
-  Serial.println(i);
-  Serial.println(j);
+  NewPing sonar1(8,2,500);
+  delay(100);
+  NewPing sonar2(13,12,500);
+  delay(100);
+  cm1 = sonar1.ping_cm();
+  cm2 = sonar2.ping_cm();
+  Serial.print("cm1=");
+  Serial.println(cm1);
+  Serial.print("cm2=");
+  Serial.println(cm2);
+  
+  analogWrite(ENB,90);
+  
+ // analogWrite(ENA,90);
 
 }
